620c17981374dadbd9539577f83cd6fcc23768c2
Creed999 <sarode.r@northeastern.edu>
2024-03-01 02:11:58 -0500

    Add conversational RAG chain for response generation

--- src/app.py
+++ src/app.py
@@ -6,13 +6,14 @@ from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.chains import create_history_aware_retriever
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 
 
 load_dotenv()
 
-def get_response(user_input):
-    return "I don't know the answer to that question."
+
+
 
 def get_vectorestore_from_url(url):
     #get the text in document form
@@ -30,6 +31,7 @@ def get_vectorestore_from_url(url):
 
 
 def get_context_retriever_chain(vector_store):
+
     llm = ChatOpenAI()
 
     retriever = vector_store.as_retriever()
@@ -44,14 +46,42 @@ def get_context_retriever_chain(vector_store):
 
     return retriever_chain
 
+#create a document chain
+def get_conversational_rag_chain(retriever_chain):
+
+    llm = ChatOpenAI()
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Answer the user's question based on the below context:\n\n{context}"),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}" ),
+    ])
+
+    stuff_documents_chain = create_stuff_documents_chain(llm, prompt)
+
+    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
+
+def get_response(user_input):
+    #create conversation chain
+    # vector_store = get_vectorestore_from_url(website_url)
+    retriever_chain = get_context_retriever_chain(st.session_state.vector_store)
+    conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
+
+    # response = get_response(user_query)
+    response = conversation_rag_chain.invoke({
+        "chat_history": st.session_state.chat_history,
+        "input": user_query
+    })
+
+    return response['answer']
+
+
+
 #app configuration
 st.set_page_config(page_title="Chat with website", page_icon="🤖")
 st.title("Chat With Websites")
 
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = [
-        AIMessage(content="Hello, I am WebWise, How can I help you today?"),
-    ]
+
 
 
 
@@ -65,16 +95,20 @@ if website_url is not None and website_url == "":
 
 else:
 
-    vector_store = get_vectorestore_from_url(website_url)
+    #session state
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content="Hello, I am WebWise, How can I help you today?"),
+        ]
+    if "vector_store" not in st.session_state:
+        st.session_state.vector_store = get_vectorestore_from_url(website_url)
 
-    retriever_chain = get_context_retriever_chain(vector_store)
 
     # document_chunks = get_vectorestore_from_url(website_url)
 
     #user input
     user_query = st.chat_input("Type your message here")
     if user_query is not None and user_query != "":
-
         response = get_response(user_query)
         st.session_state.chat_history.append(HumanMessage(content=user_query))
         st.session_state.chat_history.append(AIMessage(content=response))
